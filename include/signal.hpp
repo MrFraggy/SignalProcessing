8cfeb77d74f8f80a2d6769887990bc8f8fc60e73
AdrienMgm <adrien.mgm@gmail.com>
2014-11-28 09:45:35 +0100

    ADD: Signal::load & Signal::save

--- include/signal.hpp
+++ include/signal.hpp
@@ -8,12 +8,15 @@
 class Signal 
 {
 public:
-	Signal(int size, int t = 0);
+	Signal(int size = 0, int t = 0);
 	Signal(const Signal& other);
 	~Signal() = default;
 
 	double& operator[](int index);
 	double operator[](int index) const;
+
+	bool load(const std::string& filename);
+	bool save(const std::string& filename);
 
 protected:
 	std::unique_ptr<double> data;
--- src/signal.cpp
+++ src/signal.cpp
@@ -1,11 +1,10 @@
 #include "signal.hpp"
 #include <cstring>
 #include <stdexcept>
+#include <fstream>
+#include <vector>
 
-Signal::Signal(int size, int t) : data(new double[size]), size(size), offset(t)
-{
-	
-}
+Signal::Signal(int size, int t) : data(new double[size]), size(size), offset(t) {}
 
 Signal::Signal(const Signal& other) : Signal(size)
 {
@@ -26,4 +25,41 @@ double Signal::operator[](int index) const
 	if(off >= size || off < 0)
 		throw std::out_of_range("out of range");
 	return data.get()[off];
+}
+
+bool Signal::load(const std::string& filename)
+{
+	std::ifstream in(filename);
+
+	if(!in || !in.is_open()) return false;
+
+	std::vector<double> tmp;
+	double d;
+
+	while(in >> d)
+	{
+		tmp.push_back(d);
+	}
+
+	data.reset(new double[tmp.size()]);
+
+	std::memcpy(data.get(), tmp.data(), sizeof(double) * tmp.size());
+	size = tmp.size();
+
+	return true;
+}
+
+bool Signal::save(const std::string& filename)
+{
+	std::ofstream out(filename);
+
+	if(!out || !out.is_open()) return false;
+
+	for(uint i = 0; i < size; ++i)
+	{
+		out << data.get()[i];
+		out << std::endl;
+	}
+
+	return true;
 }
